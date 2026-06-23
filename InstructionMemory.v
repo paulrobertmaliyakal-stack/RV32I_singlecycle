27b2dfc3ea73c9a824ab7addc62bac67d7fa5a68
paul <paulrobertx14@gmail.com>
2026-06-23 21:21:43 +0530

    initial Commit

---
+++ InstructionMemory.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.06.2026 21:20:28
+// Design Name: 
+// Module Name: InstructionMemory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module InstructionMemory(
+    input [31:0] PC_IN,
+    output [31:0] INSTR_OUT,
+    input CLK
+    );
+endmodule
---
+++ PC.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.06.2026 21:20:28
+// Design Name: 
+// Module Name: PC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC(
+    input [31:0] ADDR_IN,
+    output [31:0] ADDR_OUT,
+    input CLK
+    );
+endmodule
